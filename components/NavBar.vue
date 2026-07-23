d4f070da24b2918ffcacf870c5c4d64e0914bfdb
LosAngelous <19857198709@163.com>
2026-07-23 16:38:25 +0800

    feat 增加栏目

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -8,6 +8,7 @@ const links = [
   { to: '/goals', label: '!TARGET!' },
   { to: '/likes', label: 'Like And Unlike' },
   { to: '/adore', label: 'Adore!!' },
+  { to: '/push', label: '又推又推' },
   { to: '/diary', label: '回忆日记' },
   { to: '/letters', label: '给未来的信' },
   { to: '/calendar', label: 'Calendar' },
