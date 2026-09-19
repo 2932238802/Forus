4389e4688fc96b07b42a04dc29dbdb941cc3c731
LosAngelous <19857198709@163.com>
2026-09-19 20:10:07 +0800

    [feat] 隐藏日历 以及 又推 又推

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -8,10 +8,8 @@ const links = [
   { to: '/goals', label: '!TARGET!' },
   { to: '/likes', label: 'Like And Unlike' },
   { to: '/adore', label: 'Adore!!' },
-  { to: '/push', label: '又推又推' },
   { to: '/dislikes', label: '需要改进的地方' },
   { to: '/letters', label: '给未来的信' },
-  { to: '/calendar', label: 'Calendar' },
 ]
 
 const open = ref(false)
